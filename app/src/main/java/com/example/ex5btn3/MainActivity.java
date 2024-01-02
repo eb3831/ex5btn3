f987e62174557deef1db4d0d6d4943b8df026809
eb3831 <eb3831@bs.amalnet.k12.il>
2024-01-02 09:12:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ex5btn3/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.ex5btn3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView tv;
+    int num = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
+        btn = findViewById(R.id.btn);
+
+    }
+
+    public void clicked(View view) {
+        num++;
+        tv.setText("this is click number: " + num);
+        if (num == 6) {
+            tv.setText("Enough to click. Go to new start!");
+            num = 0;
+        }
+    }
+}
